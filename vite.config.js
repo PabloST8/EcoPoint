c838ec51b9c6678fb9455dcb2e1c37a0550d04b7
PabloST8 <pablofafstar@gmail.com>
2025-06-20 22:00:49 -0300

    aaa

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/EcoPoint/', // 👈 isso aqui é essencial!
+  base: '/EcoPoint/', // ESSENCIAL!
   plugins: [react(), tailwindcss()],
 })
